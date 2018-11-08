a2d5736e53f1982e16500f2e2e96615c86f3dcb7
Sherlock <779493113@qq.com>
2018-11-08 14:08:43 +0800

    Initial Commit

---
+++ d/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  d
+//
+//  Created by lyt on 2018/11/8.
+//  Copyright © 2018年 吴宏佳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
